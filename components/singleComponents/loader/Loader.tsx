5ac1475573b6b177fe1b82125a30d04f04242644
nikolai-sim <nikolai_sim@hotmail.com>
2022-08-22 11:38:19 +1200

    add working prelaoder

--- components/singleComponents/loader/Loader.tsx
+++ components/singleComponents/loader/Loader.tsx
@@ -5,16 +5,18 @@ export default function Loader() {
     <div>
       <div className="container">
         <div className="container_inner">
-          <h1>
-            <span className="let1">l</span>
-            <span className="let2">o</span>
-            <span className="let3">a</span>
-            <span className="let4">d</span>
-            <span className="let5">i</span>
-            <span className="let6">n</span>
-            <span className="let7">g</span>
-          </h1>
           <div className="Loader"></div>
+          <div style={{ alignSelf: "flex-end" }}>
+            <h2>
+              <span className="let1">l</span>
+              <span className="let2">o</span>
+              <span className="let3">a</span>
+              <span className="let4">d</span>
+              <span className="let5">i</span>
+              <span className="let6">n</span>
+              <span className="let7">g</span>
+            </h2>
+          </div>
         </div>
       </div>
     </div>
